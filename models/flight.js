804975f94d83b9512c2d51aabf38b4aa38f34d6d
AlexG <Alexanderg.savitr@gmail.com>
2024-08-11 20:54:47 -0500

    Add some styling and ticket Model

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,19 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0,
+  }}, 
+  {
+    timestamps: true
+})
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -21,7 +34,8 @@ const flightSchema = new Schema({
   },
   departs: {
     type: Date,
-  }
+  },
+  tickets: [ticketSchema]
 })
 
 const Flight = mongoose.model('Flight', flightSchema)
